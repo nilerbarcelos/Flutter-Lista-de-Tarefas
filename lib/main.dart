273025f3b2c015a8bde40a6833e99739d4e0bc6a
*Niler <nilerbarcelos@gmail.com>
2020-05-05 15:15:31 -0300

    Adicionando textField e buttom para adicionar tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List _todoList = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("Add"),
+                  textColor: Colors.white,
+                  onPressed: () {},
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
@@ -37,14 +67,11 @@ class _HomeState extends State<Home> {
   }
 
   Future<String> _readData() async {
-    try{
+    try {
       final file = await _getFile();
       return file.readAsString();
     } catch (e) {
       return null;
     }
   }
-
 }
-
-
