68eedc8a2e205a7b5aa1212ac45bcb374de5752e
*Niler <nilerbarcelos@gmail.com>
2020-05-07 14:49:06 -0300

    Ordenação das tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,22 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b) {
+        if(a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,10 +91,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: buildItem,
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
             ),
           ),
         ],
@@ -134,6 +153,7 @@ class _HomeState extends State<Home> {
               duration: Duration(seconds: 2),
             );
 
+            Scaffold.of(context).removeCurrentSnackBar();
             Scaffold.of(context).showSnackBar(snack);
           });
         });
